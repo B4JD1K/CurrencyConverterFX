0923bc078d99e110fdfbd67330313c34de98e3a5
B4JD1K <bartlomiej.noworyta.96@gmail.com>
2023-11-20 16:12:58 +0100

    Added few variables, added generic type to ComboBox<> renamed these variables.

--- src/main/java/com/example/currencyconverterfx/ConverterController.java
+++ src/main/java/com/example/currencyconverterfx/ConverterController.java
@@ -5,12 +5,21 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
+
 public class ConverterController {
     @FXML
     public TextField enterAmount;
     @FXML
-    public ComboBox fromCurrency, toCurrency;
+    public ComboBox<String> currencyFromBox, currencyToBox;
     @FXML
     public Label convertedAmount;
+
+    private String currencyFrom, currencyTo, apiKey;
+
+    private ArrayList<String> currencyList;
+
+
+
 
 }
