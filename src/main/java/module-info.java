d95bedc63869a73d75004bd2f2f7d93effc08338
B4JD1K <bartlomiej.noworyta.96@gmail.com>
2023-11-21 16:30:18 +0100

    updated module-info.java with required libraries

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.example.currencyconverterfx {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires okhttp3;
+    requires com.google.gson;
 
     opens com.example.currencyconverterfx to javafx.fxml;
     exports com.example.currencyconverterfx;
